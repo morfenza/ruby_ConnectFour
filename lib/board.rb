099080a8be3f4b12d4eea32696615dcac26210e3
Morfenza <ianlourenco@protonmail.com>
2023-03-27 17:55:04 -0300

    Implement #display_rack

--- lib/board.rb
+++ lib/board.rb
@@ -25,4 +25,26 @@ class Board
   def already_taken?(row, column)
     !@rack[row][column].zero?
   end
+
+  # rubocop:disable Metrics/MethodLength
+  def display_rack
+    filled = "\u25cf"
+    unfilled = "\u25cb"
+    @rack.each do |row|
+      row.each do |column|
+        print "\e[31m#{filled}\e[0m " if column == 1 # red
+        print "\e[32m#{filled}\e[0m " if column == 2 # green
+        print "#{unfilled} " if column.zero? # transparent
+      end
+      puts
+    end
+    puts '1 2 3 4 5 6 7'
+  end
 end
+# rubocop:enable Metrics/MethodLength
+
+# a = Board.new
+# a.create_rack
+# a.insert_token(2, 2, 2)
+# a.insert_token(1, 3, 3)
+# a.display_rack
